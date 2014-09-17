dd585c0015f1cd6d5fab9c1f5c231f7b2abca916
Somaiah Kumbera <somaiah.kumbera@capgemini.com>
2014-09-17 20:40:47 +0200

    PhraseFinder initial commit

--- src/main/java/com/capgemini/phrasefinder/MyApplicationConfiguration/PhaseFinderAppConfiguration.java
+++ src/main/java/com/capgemini/phrasefinder/MyApplicationConfiguration/PhaseFinderAppConfiguration.java
@@ -2,19 +2,16 @@ package com.capgemini.phrasefinder.MyApplicationConfiguration;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
-import javax.validation.constraints.Max;
 import org.hibernate.validator.constraints.NotEmpty;
 
 /**
  * @author Somaiah Kumbera (Capgemini)
- *
  */
 public class PhaseFinderAppConfiguration extends Configuration {
 
     @JsonProperty
     @NotEmpty
     private String dbURL;
-
 
     public String getDbURL() {
         return dbURL;
